7259142df3c2b8ab08791d3b0608743e70ed2ebc
‘Nicolas’ <nicolas.glinkin@gmail.com>
2022-12-02 12:53:00 +0200

    code refactoried with XLMHttpRequest Mozilla FAQ

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 const myUrl = 'https://bersoantik.com/ru/catalog/article/25_281/'
-const xhr = new XMLHttpRequest()
 
-xhr.open("get", myUrl)
-
-xhr.send()
+function reqListener () {
+    console.log(this.responseText);
+  }
+  
+  var oReq = new XMLHttpRequest();
+  oReq.onload = reqListener;
+  oReq.open("get", myUrl, true);
+  oReq.send();
+  
