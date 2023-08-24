e68244cfcedcb3efb0b579660d5ed4c93e93a080
nicolas-gl <nicolas.glinkin@gmail.com>
2023-08-24 23:07:53 +0300

    prices correct now when item discounted

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,4 +1,4 @@
-import {useState, useLayoutEffect} from 'react'
+import { useState, useLayoutEffect } from 'react'
 import React from 'react'
 import styles from './Text.module.scss'
 
@@ -9,20 +9,21 @@ export default function Text(props) {
   const [item, setItem] = useState();
   const [error, setError] = useState();
 
-  useLayoutEffect(() => { if (props.isSelected) {
-    setRequest("loading")
-    fetch(`https://pricetags.tech/articles/${props.article}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setRequest("ok")
-          setItem(result);
-        },
-        (error) => {
-          setRequest("error");
-          setError(error);
-        }
-      )
+  useLayoutEffect(() => {
+    if (props.isSelected) {
+      setRequest("loading")
+      fetch(`https://pricetags.tech/articles/${props.article}`)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setRequest("ok")
+            setItem(result);
+          },
+          (error) => {
+            setRequest("error");
+            setError(error);
+          }
+        )
     } else {
       setRequest("empty");
       setItem();
@@ -30,7 +31,7 @@ export default function Text(props) {
     }
   }, [props.isSelected, props.article])
 
-  
+
   if (request === "empty") {
     return <div></div>
   } else if (request === "loading") {
@@ -42,28 +43,39 @@ export default function Text(props) {
       <div className={styles["text-form"]}>
         <ul className={styles.text}>
 
-          <li className={styles.title} key={styles.title} contentEditable>
+          <li className={styles.title} contentEditable>
             {item.title}
           </li>
 
-          <li className={styles.material} key={styles.material} contentEditable>
-            {( item.material[0] ? item.material[0].toUpperCase() + item.material.slice(1) : '' ) }
+          <li className={styles.material} contentEditable>
+            {(item.material[0] ? item.material[0].toUpperCase() + item.material.slice(1) : '')}
           </li>
 
-          <li className={styles.period} key={styles.period} contentEditable>
-            {item.period} 
+          <li className={styles.period} contentEditable>
+            {item.period}
           </li>
 
-          <li className={styles.price} key={styles.price} contentEditable>
-            Цена: {item.price} руб.
+          {item.last_price ?
+            <>
+              <li className={styles.lastPrice} contentEditable>
+                Цена: <span className={styles.text}>{item.last_price} руб.</span>
+              </li>
+              <li className={styles.discountPrice} contentEditable>
+                {item.price} руб.
+              </li>
+            </>
+            :
+            <li className={styles.price} contentEditable>
+              Цена: {item.price} руб.
+            </li>
+          }
+
+          <li className={styles.article_id} contentEditable>
+            Арт. {item.article_id}
           </li>
 
-          <li className={styles.article_id} key={styles.article_id} contentEditable>
-            Арт. {item.article_id} 
-          </li>
-          
         </ul>
-      </div>
+      </div >
     );
   }
 }
